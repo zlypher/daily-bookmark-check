02f343e92ddffa9f9b1fd369e90ac07fc929f3f6
Thomas Prochazka <thomas.prochazka@gmail.com>
2020-05-05 20:35:12 +0200

    Add link to open bookmark in another tab

--- src/components/bookmark-item.js
+++ src/components/bookmark-item.js
@@ -1,16 +1,18 @@
 import { h } from "preact";
 import { Toggle } from "./toggle";
 import "./bookmark-item.scss";
+import { ExternalLink } from "./external-link";
 
 export const BookmarkItem = (props) => {
   const { bookmark, status, toggleDelete } = props;
   return (
     <div className={`bookmark-item js-bookmark-item bookmark-item--${status}`}>
+      <ExternalLink className="bookmark-item__external" href={bookmark.url} />
       <div className="bookmark-item__wrapper">
         <div className="bookmark-item__title" title={bookmark.title}>
           {bookmark.title}
         </div>
-        <div className="bookmark-item__url" title="{bookmark.url">
+        <div className="bookmark-item__url" title={bookmark.url}>
           {bookmark.url}
         </div>
       </div>
